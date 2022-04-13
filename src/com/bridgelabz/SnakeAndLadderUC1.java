d03b6359d41b3d0eaa502cda678937ad750f3c67
iaksharhate <iaksharhate@gmail.com>
2022-04-13 16:53:50 +0530

    UC1: Player starting from first position

---
+++ src/com/bridgelabz/SnakeAndLadderUC1.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderUC1 {
+    public static void main(String[] args) {
+        int position = 0;
+
+        System.out.println("Welcome to Snake & Ladder Game!!");
+        System.out.println("Your are at starting position: "+position);
+    }
+}
